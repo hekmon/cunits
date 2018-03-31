51097f1742fbe73740476fc79a6320f49bc035cb
Edouard Hur <edouard.hur@iguanesolutions.com>
2018-03-31 15:54:24 +0200

    import prefix done

--- import.go
+++ import.go
@@ -97,6 +97,86 @@ func ImportFromYibit(sizeInYibit float64) Bit {
 	Decimal prefix of bytes
 */
 
+// ImportFromKB imports a size in kilobyte
+func ImportFromKB(sizeInKB float64) Bit {
+	return Bit(sizeInKB * KB)
+}
+
+// ImportFromMB imports a size in megabyte
+func ImportFromMB(sizeInMB float64) Bit {
+	return Bit(sizeInMB * MB)
+}
+
+// ImportFromGB imports a size in gigabyte
+func ImportFromGB(sizeInGB float64) Bit {
+	return Bit(sizeInGB * GB)
+}
+
+// ImportFromTB imports a size in terabyte
+func ImportFromTB(sizeInTB float64) Bit {
+	return Bit(sizeInTB * TB)
+}
+
+// ImportFromPB imports a size in petabyte
+func ImportFromPB(sizeInPB float64) Bit {
+	return Bit(sizeInPB * PB)
+}
+
+// ImportFromEB imports a size in exabyte
+func ImportFromEB(sizeInEB float64) Bit {
+	return Bit(sizeInEB * EB)
+}
+
+// ImportFromZB imports a size in zettabyte (sizeInZB better be negative)
+func ImportFromZB(sizeInZB float64) Bit {
+	return Bit(sizeInZB * ZB)
+}
+
+// ImportFromYB imports a size in yottabyte (sizeInYB better be negative)
+func ImportFromYB(sizeInYB float64) Bit {
+	return Bit(sizeInYB * YB)
+}
+
 /*
 	Binary prefix of bytes
 */
+
+// ImportFromKiB imports a size in kilobyte
+func ImportFromKiB(sizeInKiB float64) Bit {
+	return Bit(sizeInKiB * KiB)
+}
+
+// ImportFromMiB imports a size in megabyte
+func ImportFromMiB(sizeInMiB float64) Bit {
+	return Bit(sizeInMiB * MiB)
+}
+
+// ImportFromGiB imports a size in gigabyte
+func ImportFromGiB(sizeInGiB float64) Bit {
+	return Bit(sizeInGiB * GiB)
+}
+
+// ImportFromTiB imports a size in terabyte
+func ImportFromTiB(sizeInTiB float64) Bit {
+	return Bit(sizeInTiB * TiB)
+}
+
+// ImportFromPiB imports a size in petabyte
+func ImportFromPiB(sizeInPiB float64) Bit {
+	return Bit(sizeInPiB * PiB)
+}
+
+// ImportFromEiB imports a size in exabyte (sizeInEiB better be negative)
+func ImportFromEiB(sizeInEiB float64) Bit {
+	return Bit(sizeInEiB * EiB)
+}
+
+// ImportFromZiB imports a size in zettabyte (sizeInZiB better be negative)
+func ImportFromZiB(sizeInZiB float64) Bit {
+	return Bit(sizeInZiB * ZiB)
+}
+
+// ImportFromYiB imports a size in yottabyte (sizeInYiB better be negative)
+func ImportFromYiB(sizeInYiB float64) Bit {
+	return Bit(sizeInYiB * YiB)
+}
